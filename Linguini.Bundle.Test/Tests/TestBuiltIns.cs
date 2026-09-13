07bad45319e7ff4759c0cf8716a17ac3025609da
Ygg01 <y.laughing.man.y@gmail.com>
2026-09-13 20:32:01 +0200

    Start working on formatting tests

---
+++ Linguini.Bundle.Test/Tests/TestBuiltIns.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Linguini.Shared.Types;
+using Linguini.Shared.Types.Bundle;
+using NUnit.Framework;
+
+namespace Linguini.Bundle.Test.Tests
+{
+    [TestFixture]
+    public class TestBuiltIns
+    {
+        public static IEnumerable<TestCaseData> TestNumberEnumerable()
+        {
+            yield return new TestCaseData(
+                0.0,
+                "en-US",
+                new FluentNumberOptions(){},
+                "0.0");
+        }
+
+        [Parallelizable]
+        [Test]
+        [TestCaseSource(nameof(TestNumberEnumerable))]
+        public void TestNumbers(double number, string langLoc, FluentNumberOptions fluentNumberOptions, string expected)
+        {
+            var fluentNumber = (FluentNumber)number;
+            var testContext = new FluentContext(langLoc, fluentNumberOptions);
+            var actual = fluentNumber.AsString(testContext);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+    
+}
