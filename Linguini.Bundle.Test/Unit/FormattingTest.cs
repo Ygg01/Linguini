104e59271f8abf5737f4a25a9955247125d5df86
Ygg01 <y.laughing.man.y@gmail.com>
2026-09-17 01:05:00 +0200

    Uncomment and restore number formatting test cases in `FormattingTest`.

--- Linguini.Bundle.Test/Unit/FormattingTest.cs
+++ Linguini.Bundle.Test/Unit/FormattingTest.cs
@@ -10,36 +10,36 @@ namespace Linguini.Bundle.Test.Unit
     {
         private static IEnumerable<TestCaseData> TestNumberFormatCases()
         {
-            // // Simple Formats
-            // yield return new TestCaseData(123, new FluentContext(LangLocId.EN, new FluentNumberOptions()
-            // {
-            //     MinimumFractionDigits = 2
-            // })).Returns("123.00");
-            //
-            // yield return new TestCaseData(123.1, new FluentContext(LangLocId.EN, new FluentNumberOptions()
-            // {
-            //     MinimumFractionDigits = 4
-            // })).Returns("123.1000");
-            //
-            // yield return new TestCaseData(11.1, new FluentContext(LangLocId.EN, new FluentNumberOptions()
-            // {
-            //     Style = FluentNumberStyle.Currency,
-            //     MinimumFractionDigits = 3
-            // })).Returns("¤11.100");
-            //
-            // // Decimal complex formats
-            // yield return new TestCaseData(123.45, new FluentContext(LangLocId.EN, new FluentNumberOptions()
-            // {
-            //     Style = FluentNumberStyle.Decimal,
-            //     MaximumFractionDigits = 1,
-            //     MinimumFractionDigits = 3,
-            // })).Returns("123.450");
-            // yield return new TestCaseData(123.4567, new FluentContext(LangLocId.EN, new FluentNumberOptions()
-            // {
-            //     Style = FluentNumberStyle.Decimal,
-            //     MaximumFractionDigits = 3,
-            //     MinimumFractionDigits = 1,
-            // })).Returns("123.457");
+            // Simple Formats
+            yield return new TestCaseData(123, new FluentContext(LangLocId.EN, new FluentNumberOptions()
+            {
+                MinimumFractionDigits = 2
+            })).Returns("123.00");
+            
+            yield return new TestCaseData(123.1, new FluentContext(LangLocId.EN, new FluentNumberOptions()
+            {
+                MinimumFractionDigits = 4
+            })).Returns("123.1000");
+            
+            yield return new TestCaseData(11.1, new FluentContext(LangLocId.EN, new FluentNumberOptions()
+            {
+                Style = FluentNumberStyle.Currency,
+                MinimumFractionDigits = 3
+            })).Returns("¤11.100");
+            
+            // Decimal complex formats
+            yield return new TestCaseData(123.45, new FluentContext(LangLocId.EN, new FluentNumberOptions()
+            {
+                Style = FluentNumberStyle.Decimal,
+                MaximumFractionDigits = 1,
+                MinimumFractionDigits = 3,
+            })).Returns("123.450");
+            yield return new TestCaseData(123.4567, new FluentContext(LangLocId.EN, new FluentNumberOptions()
+            {
+                Style = FluentNumberStyle.Decimal,
+                MaximumFractionDigits = 3,
+                MinimumFractionDigits = 1,
+            })).Returns("123.457");
             
             // Currency complex formats
             yield return new TestCaseData(123.4567, new FluentContext("en-US", new FluentNumberOptions()
